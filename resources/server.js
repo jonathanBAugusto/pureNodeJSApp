66fc02c2de1aa3faf2a180b33e64c6b1de35a5f1
Jhon Doe Dev <jonathanbersot21@gmail.com>
2021-02-25 00:29:03 -0300

    noEndpoint to notFound;

--- resources/server.js
+++ resources/server.js
@@ -25,7 +25,7 @@ const uniServer = (req, res) => {
   req.on('end', () => {
     buffer += decoder.end();
 
-    let chosenHandler = typeof (routes[trimmedPath]) !== 'undefined' ? routes[trimmedPath] : handlers.noEndpoint;
+    let chosenHandler = typeof (routes[trimmedPath]) !== 'undefined' ? routes[trimmedPath] : handlers.notFound;
 
     const data = {
       'trimmedPath': trimmedPath,
